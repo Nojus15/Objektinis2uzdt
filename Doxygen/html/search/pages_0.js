2eec89ffda9871f5bbba654f8ac7b02df3c16e3a
Nojus <53999653+Nojus15@users.noreply.github.com>
2022-05-09 18:27:36 +0300

    Doxygen

---
+++ Doxygen/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['objektinis2uzdt_0',['Objektinis2uzdt',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ Doxygen/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "agostvz",
+  1: "agstvz",
+  2: "o"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Pages"
+};
+
